f4d81cd5d6b8662342deccc2d39f318aac3af818
ja54312 <ija54312@gmail.com>
2021-07-04 12:52:35 -0500

    modificacion margin

--- src/ServicioTecnico.js
+++ src/ServicioTecnico.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import  './ServicioTecnico.css';
 import servicio1 from './arquivos/SERVICE-WEB-03.png'
 import servicio2 from './arquivos/SERVICE-WEB-04.png'
@@ -8,6 +9,7 @@ import servicio3 from './arquivos/SERVICE-WEB-05.png'
 export default function ServicioTecnico(){
     return(
         <>
+    <div className="Servicio-Tecncico">
         <div className="title">
             <h1>
                 <strong>servicio</strong> técnico autorizado
@@ -15,21 +17,21 @@ export default function ServicioTecnico(){
         </div>
         <div className="container-logos">
             <div className="logo-one">
-                <img src={servicio1} alt="imagen de factura"/>
+                <img src={servicio1} alt="imagen de horario"/>
                 <p>
                 No necesitás presentar <br/> tu <strong>factura de compra</strong>
                 </p>
             </div>
             <hr/>
             <div className="logo-two">
-                <img src={servicio2} alt="imagen de factura"/>
+                <img src={servicio2} alt="imagen de calendario"/>
                 <p>
                 Lo reparamos en <br/>  <strong>menos de 72 horas</strong>
                 </p>
             </div>
             <hr/>
             <div className="logo-three">
-                <img src={servicio3} alt="imagen de factura"/>
+                <img src= {servicio3} alt="imagen de factura"/>
                 <p>
                 Tenés <strong> 90 días de garantía</strong>  <br/> en la reparación
                 </p>
@@ -51,6 +53,9 @@ export default function ServicioTecnico(){
                 <span className="number-contact"><strong>2903 1876</strong></span>
             </div>
         </div>
+    </div>
         </>
     );
 }
+const entryPointMalta = document.getElementById("root");
+ReactDOM.render(<ServicioTecnico/>, entryPointMalta);
