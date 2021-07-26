95612c9874a438d4304af806beab492b6b6c0092
ja54312 <ija54312@gmail.com>
2021-07-26 16:48:13 -0500

    Seccion maquetada sku selector

--- src/EdgeLite.js
+++ src/EdgeLite.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import './CSS/EdgeLite.css';
-import Logo from './arquivos/Grupo_2196.svg';
+
 
 
 export default function EdgeLite(){
@@ -11,11 +11,65 @@ export default function EdgeLite(){
             <div className="Logo-header-container"></div>
             <nav>
                 <li className="li-1">Overview</li>
-                <li>Tech Specs</li>
-                <button>Buy now</button>
+                <li className="li-2">Tech Specs</li>
+                <button className="button-nav">Buy now</button>
             </nav>
         </header>
-        <body></body>
+        <body>
+            <section className="Section-sku-selector">
+                <div className="Sku-selector-info">
+                    <div className="sku-title">
+                        <span className="sku-title-one">Your next edge phone is here</span><br></br><br></br>
+                        <span className="sku-text">Lorem ipsum dolor sit amet.</span>
+                    </div>
+                    <div className="container-device-selector">
+                        <div className="title-device">
+                            <span>Choose a device</span>
+                        </div>
+                        <div className="containers-device">
+                            <button className="button-device"><span>edge 20 pro</span></button>
+                            <button className="button-device"><span>edge 20 fusion</span></button>
+                            <button className="button-device"><span>edge 20</span></button>
+                        </div>
+                    </div>
+                    <div className="container-color-selector">
+                        <div className="title-color-selector">
+                            <span>Choose a color</span>
+                        </div>
+                        <div className="containers-color-selector">
+                            <button className="button-color-selector btn-1"></button>
+                            <button className="button-color-selector btn-2"></button>
+                            <button className="button-color-selector btn-3"></button>
+                        </div>
+                    </div>
+                    <div className="container-storage-selector">
+                    <div className="title-storage">
+                            <span>Storage option</span>
+                        </div>
+                        <div className="containers-storage">
+                            <button className="button-storage"><span>128GB</span></button>
+                           <span className="span-carrier">Carrier</span>
+                            <button className="button-storage-carrier"><span>Unlocked</span></button>
+                        </div>
+                    </div>
+                    <div className="container-price">
+                    <span className="Price">$699.99</span>
+                    </div>
+                    <div className="container-button-sku">
+                    <button className="button-sku">Buy the Motorola Edge pro</button>
+                    </div>
+                </div>
+                <div className="container-sku-image">
+                    <span className="flecha-izq"></span>
+                    <div className="sku-image"></div>
+                    <span className="flecha-der"></span>
+                </div>
+            </section>
+            <section className="container-edge-colors">
+                <div className="container-text-edge-colors"></div>
+                <div className="container-image-edge-colors"></div>
+            </section>
+        </body>
         <footer></footer>
    
         </>
