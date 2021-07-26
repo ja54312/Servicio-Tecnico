d92f6510c9e89b9e5420801cf1e0591e29b4b751
ja54312 <ija54312@gmail.com>
2021-07-26 18:02:15 -0500

    Se agregaron imagenes y se termino seccion periscope

--- src/EdgeLite.js
+++ src/EdgeLite.js
@@ -97,6 +97,17 @@ export default function EdgeLite(){
                     </div>
                 </div>
             </section>
+            <section className="container-periscope">
+                <div className="periscope-image"></div>
+                <div className="periscope-text-container">
+                    <div className="title-periscope">
+                        <span>Periscope lens</span>
+                    </div>
+                    <div className="text-periscope">
+                        <p>The periscope lens system allows you to capture objects from far and beyond with amazing clarity.</p>
+                    </div>
+                </div>
+            </section>
         </body>
         <footer></footer>
    
