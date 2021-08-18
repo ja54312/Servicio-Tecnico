36e8c19268bccb5bd9d9360e5b8a6aee833c9ad2
ja54312 <ija54312@gmail.com>
2021-08-18 16:40:18 -0500

    buidl

--- final/ServicioTecnico.ead25f3b.js
+++ final/ServicioTecnico.ead25f3b.js
@@ -29742,7 +29742,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39559" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38273" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
